0696fcd9740a207fb5281c8c26a0c7a914f89ddc
polylabs <hungnttg@gmail.com>
2020-05-22 08:50:34 +0700

    Update ChangPassMySQL.sql

--- ChangPassMySQL.sql
+++ ChangPassMySQL.sql
@@ -1,9 +1,2 @@
--- Create a user named mgs_user
-GRANT SELECT, INSERT, UPDATE, DELETE
-ON *
-TO root@localhost
-IDENTIFIED BY '123';
-
-
 UPDATE mysql.user SET Password=PASSWORD('MyNewPass') WHERE User='root';
 FLUSH PRIVILEGES;
