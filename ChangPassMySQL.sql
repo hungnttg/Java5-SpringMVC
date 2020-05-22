5614a428646d5637a3895df067fe4bba07c844bf
polylabs <hungnttg@gmail.com>
2020-05-22 21:36:02 +0700

    Update ChangPassMySQL.sql

--- ChangPassMySQL.sql
+++ ChangPassMySQL.sql
@@ -1,7 +1,10 @@
-UPDATE mysql.user SET Password=PASSWORD('MyNewPass') WHERE User='root';
-FLUSH PRIVILEGES;
+--Thay doi password tren phpmyadmin
+mysql> create database db_example; -- Creates the new database
+mysql> create user 'springuser'@'%' identified by 'ThePassword'; -- Creates the user
+mysql> grant all on db_example.* to 'springuser'@'%'; -- Gives all privileges to the new user on the newly created database
 
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'MyNewPass';
-
-mysqladmin.exe -u root password WhateverPassword
+----application.properties
+spring.jpa.hibernate.ddl-auto=update
+spring.datasource.url=jdbc:mysql://${MYSQL_HOST:localhost}:3306/db_example
+spring.datasource.username=springuser
+spring.datasource.password=ThePassword
